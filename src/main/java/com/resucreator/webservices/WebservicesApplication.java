dc2cc1a203db96229efa2550f121baf0da260d26
tirthyakamaldasgupta <tirthyakamaldasgupta@gmail.com>
2022-12-07 08:46:04 +0530

    Added springdoc-openapi 2.0.0 as a Maven dependency. Configured the application to show the API documentation via Swagger UI.

--- src/main/java/com/resucreator/webservices/WebservicesApplication.java
+++ src/main/java/com/resucreator/webservices/WebservicesApplication.java
@@ -1,11 +1,12 @@
 package com.resucreator.webservices;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition
 public class WebservicesApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(WebservicesApplication.class, args);
 	}
