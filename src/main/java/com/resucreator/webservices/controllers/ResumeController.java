25e8ff8d1041d9a6b928cf3cd815e92ce3e4e59e
tirthyakamaldasgupta <tirthyakamaldasgupta@gmail.com>
2022-12-07 09:26:06 +0530

    Removed the usage of field injection and introduced constructor injection.

--- src/main/java/com/resucreator/webservices/controllers/ResumeController.java
+++ src/main/java/com/resucreator/webservices/controllers/ResumeController.java
@@ -2,7 +2,6 @@ package com.resucreator.webservices.controllers;
 
 import com.resucreator.webservices.collections.Resume;
 import com.resucreator.webservices.services.ResumeService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,8 +11,11 @@ import java.util.List;
 
 @RestController
 public class ResumeController {
-    @Autowired
-    private ResumeService resumeService;
+    private final ResumeService resumeService;
+
+    public ResumeController(ResumeService resumeService) {
+        this.resumeService = resumeService;
+    }
 
     @GetMapping(path = "/resumes")
     public List<Resume> getResumes() {
--- src/main/java/com/resucreator/webservices/services/ResumeServiceImpl.java
+++ src/main/java/com/resucreator/webservices/services/ResumeServiceImpl.java
@@ -2,15 +2,17 @@ package com.resucreator.webservices.services;
 
 import com.resucreator.webservices.collections.Resume;
 import com.resucreator.webservices.repositories.ResumeRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class ResumeServiceImpl implements ResumeService{
-    @Autowired
-    private ResumeRepository resumeRepository;
+    private final ResumeRepository resumeRepository;
+
+    public ResumeServiceImpl(ResumeRepository resumeRepository) {
+        this.resumeRepository = resumeRepository;
+    }
 
     @Override
     public String createResume(Resume resume) {
